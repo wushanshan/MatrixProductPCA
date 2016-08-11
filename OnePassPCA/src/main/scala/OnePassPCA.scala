b264577d71e3a986371ae642655e58d505d4dfb1
Shanshan Wu <wushanshan0701@gmail.com>
2016-08-11 15:27:59 -0500

    Update OnePassPCA.scala

--- OnePassPCA/src/main/scala/OnePassPCA.scala
+++ OnePassPCA/src/main/scala/OnePassPCA.scala
@@ -26,6 +26,7 @@ import breeze.linalg.{DenseVector => BDV, SparseVector => BSV, Vector => BV,
   axpy=>brzAxpy, sum, trace, accumulate=>cumsum, DenseMatrix => BDM, Axis}
 import breeze.numerics.{round}
 import spire.syntax.cfor._
+import scala.collection.mutable.ArrayBuffer
 
 object OnePassPCA { 
   /*
@@ -69,12 +70,12 @@ object OnePassPCA {
   }
   
   /*
-   *  The following function computes matrix vector multiplication for A^TB.
-   *  It outputs B^TAA^TBv, and we will use it to get the spectral norm of A^TB.
-   *  Note that this function is slightly different from that in TwoPassLELA
-   *  because of the different ways that A and B are stored.
+   * The following functions compute matrix vector multiplications for A^TB and B^TB.
+   * We will use them to compute the spectral norms.
+   * Note that this function is slightly different from that in OnePassPCA 
+   * because of the different ways that A and B are stored.
    */
-  def matVecMulti(rawData: RDD[(Int, (BDM[Double], BDM[Double]))], v: BDV[Double], n: Int, d: Int, sc: org.apache.spark.SparkContext): BDV[Double] = {
+  def AtBv(rawData: RDD[(Int, (BDM[Double], BDM[Double]))], v: BDV[Double], n: Int, d: Int, sc: org.apache.spark.SparkContext): BDV[Double] = {
     val vBC = sc.broadcast(v)
     val vtB = rawData.treeAggregate(BDV.zeros[Double](d))(
       (acc, value) => {
@@ -95,31 +96,67 @@ object OnePassPCA {
         acc},
       (acc1, acc2) => acc1+acc2
     )
-    val AtBvBC = sc.broadcast(AtBv)
-    val AAtBv = rawData.treeAggregate(BDV.zeros[Double](d))(
+    AtBv
+  }
+  def BtAv(rawData: RDD[(Int, (BDM[Double], BDM[Double]))], v: BDV[Double], n: Int, d: Int, sc: org.apache.spark.SparkContext): BDV[Double] = {
+    val vBC = sc.broadcast(v)
+    val vtA = rawData.treeAggregate(BDV.zeros[Double](d))(
       (acc, value) => {
         val rowIndex = value._1
         val numRow = value._2._1.rows
-        val vtA = AtBvBC.value.slice(rowIndex*numRow, (rowIndex+1)*numRow).t*value._2._1
+        val vtA = vBC.value.slice(rowIndex*numRow, (rowIndex+1)*numRow).t*value._2._1
         brzAxpy(1.0, vtA.t, acc)
         acc},
       (acc1, acc2) => acc1+acc2
     )
-    val AAtBvBC = sc.broadcast(AAtBv)
-    val BtAAtBv = rawData.treeAggregate(BDV.zeros[Double](n))(
+    val vtABC = sc.broadcast(vtA)
+    val BtAv = rawData.treeAggregate(BDV.zeros[Double](n))(
       (acc, value) => {
         val rowIndex = value._1
         val numRow = value._2._1.rows
-        val Btv = value._2._2*AAtBvBC.value
+        val Btv = value._2._2*vtABC.value
         acc(rowIndex*numRow to (rowIndex+1)*numRow-1) := Btv
         acc},
       (acc1, acc2) => acc1+acc2
     )
-    BtAAtBv
+    BtAv
   }
   
+   /*
+    * The following function implements the (unnormalized) fast Walsh-Hadamard transform. 
+    * The output is in 'sequency' order, note that any ordering works here
+    * The length of the input vector is some power of 2
+    * The code is adapted from Matlab's function 'fwht'
+    */
+   def fwht(v: Array[Double]): Array[Double] = {
+    val N = v.size
+    var x = Array.range(0,N).map{i=> if(i%2==0) v(i)+v(i+1) else v(i-1)-v(i)}
+    var L = 1
+    val numLog2 = 31 - Integer.numberOfLeadingZeros(N) 
+    val y = Array.fill(N)(0.0)
+    cfor(1)(i=>i<numLog2, i=>i+1){i=>
+      var M = Math.pow(2,L).toInt
+      var J = 0
+      var K = 0
+      while (K<N){
+        cfor(J)(j=>j<J+M, j=>j+2){j=>
+          y(K) = x(j)+x(j+M)
+          y(K+1) = x(j)-x(j+M)
+          y(K+2) = x(j+1)-x(j+1+M)
+          y(K+3) = x(j+1)+x(j+1+M)
+          K += 4
+        }
+        J += 2*M
+      }
+      x = y.clone()
+      L += 1
+    }
+    y
+    //(BDV(y):*=(1.0/Math.sqrt(N))).toArray //normalize the result, norm(y)=norm(v)
+  }
+    
   def main(args: Array[String]){
-    //val args = Array("50000","50000","5","25","1000","10","0","4")
+    //val args = Array("100000","100000","5","80","2000","10","0","2")
     val n = args(0).toInt // number of columns of A & B
     val d = args(1).toInt // number of rows of A & B
     val r = args(2).toInt // desired rank
@@ -142,7 +179,7 @@ object OnePassPCA {
      */  
     println(s"Generate two random matrices A and B...")
     val rowsPerPart = n/p // number of rows per partition
-    val alpha = 0.5
+    val alpha = 1
     val rawData = sc.parallelize(Array.range(0,p),p).map{x=> 
       val rng = new Random(x).self
       val BDMA = new BDM(rowsPerPart,d, DenseMatrix.randn(rowsPerPart, d,rng).toArray)
@@ -164,22 +201,28 @@ object OnePassPCA {
      * colnSum, colnSumTilde: BDV, column norm of matrix B and B_tilde; 
      */
     println(s"Step 2: Build sketches and compute column norms...")
-    println(s"Generate a sketching matrix and broadcast it to all nodes...")
-    val now = System.currentTimeMillis
-    val rng = new Random(now).self
-    val randomMat = new BDM(d,lowD,DenseMatrix.randn(d, lowD, rng).toArray.map(x=>x/math.sqrt(lowD)))
-    val randomMatBC = sc.broadcast(randomMat) // limited to 2GB size by Spark
-    
-    println(s"Compute the sketches...")
-    val rawDataSketchRDD =  rawData.mapValues{BDMs =>
+    println(s"Apply the Subsampled Randomized Hadamard Transform (SRHT) to the input...")
+    val padZeros = if((d & -d)==d) d else 1<<(32-Integer.numberOfLeadingZeros(d)) // nearest power of 2
+    val signBC = sc.broadcast(BDV.rand(d).map(x=> if(x>0.5) 1.0 else -1.0)) // random vector with +1/-1
+    val indexBC = sc.broadcast(Random.shuffle(0 to padZeros-1).take(lowD).toArray) //random indices range from 0 to d-1
+    val rawDataSketchRDD = rawData.mapValues{BDMs =>
       val numRow = BDMs._1.rows
       val rowSum = BDV(Array.tabulate(numRow)(i=> BDMs._1(i,::) dot BDMs._1(i,::)))
       val colnSum = BDV(Array.tabulate(numRow)(i=> BDMs._2(i,::) dot BDMs._2(i,::)))
-     ((BDMs._1*randomMatBC.value, BDMs._2*randomMatBC.value), (rowSum, colnSum))
+      val BDMAsketch = new BDM(lowD, numRow, Array.tabulate(numRow){i=>
+        val ht = fwht((BDMs._1(i,::).t:*signBC.value).toArray++Array.fill(padZeros-d)(0.0))
+        val rescale = 1.0/Math.sqrt(lowD)
+        indexBC.value.map(j=>ht(j)*rescale)
+      }.flatten)
+      val BDMBsketch = new BDM(lowD, numRow, Array.tabulate(numRow){i=>
+        val ht = fwht((BDMs._2(i,::).t:*signBC.value).toArray++Array.fill(padZeros-d)(0.0))
+        val rescale = 1.0/Math.sqrt(lowD)
+        indexBC.value.map(j=>ht(j)*rescale)
+      }.flatten)
+      ((BDMAsketch.t, BDMBsketch.t), (rowSum, colnSum))
     }.cache()
     rawDataSketchRDD.count()
-    val rawDataSketch = rawDataSketchRDD.mapValues(x=>x._1).collectAsMap()
-    
+
     println(s"Compute the norms...")
     val rowColnSum = rawDataSketchRDD.mapValues(x=>x._2).collectAsMap()
     val rowColnSumTilde = rawDataSketchRDD.mapValues{BDMs =>
@@ -188,16 +231,11 @@ object OnePassPCA {
       val colnSum = BDV(Array.tabulate(numRow)(i=> BDMs._1._2(i,::) dot BDMs._1._2(i,::)))
       (rowSum, colnSum)
     }.collectAsMap()
-    var rowSum = BDV.rand(0)
-    var colnSum = BDV.rand(0)
-    var rowSumTilde = BDV.rand(0)
-    var colnSumTilde = BDV.rand(0)
-    cfor(0)(i=>i<p,i=>i+1){i=>
-      rowSum = BDV.vertcat(rowSum, rowColnSum(i)._1)
-      colnSum = BDV.vertcat(colnSum, rowColnSum(i)._2)
-      rowSumTilde = BDV.vertcat(rowSum, rowColnSumTilde(i)._1)
-      colnSumTilde = BDV.vertcat(colnSum, rowColnSumTilde(i)._2)
-    }
+    val rowSum = BDV(Array.tabulate(p)(i=>rowColnSum(i)._1.toArray).flatten)
+    val colnSum = BDV(Array.tabulate(p)(i=>rowColnSum(i)._2.toArray).flatten)
+    val rowSumTilde = BDV(Array.tabulate(p)(i=>rowColnSumTilde(i)._1.toArray).flatten)
+    val colnSumTilde = BDV(Array.tabulate(p)(i=>rowColnSumTilde(i)._2.toArray).flatten)
+ 
     
     /*
      * Step 3: Perform biased sampling.
@@ -241,19 +279,38 @@ object OnePassPCA {
      * Step 4: Construct the sampled matrix.
      * Output: RDD[Rating(rowIndex, colnIndex, value)]
      */
-    println(s"Calculate the sampled values using scala parallel collection...")
-    val samples = sampleLocations.toList.par.map{v=>
-      val value = Array.fill(v._2.size)(0.0)
-      val vecA = rawDataSketch(v._1/rowsPerPart)._1(v._1-v._1/rowsPerPart*rowsPerPart,::)
-      cfor(0)(i=>i<v._2.size, i=>i+1){i=> 
-        val vecB = rawDataSketch(v._2(i)/rowsPerPart)._2(v._2(i)-v._2(i)/rowsPerPart*rowsPerPart,::)
-        value(i) = (vecA dot vecB)*math.sqrt(rowSum(v._1)*colnSum(v._2(i)))/math.sqrt(rowSumTilde(v._1)*colnSumTilde(v._2(i)))
+    println(s"Collect sub-matrices of A and broadcast it; each node can then compute the sampled values...")
+    val samplesBuf = new ArrayBuffer[Rating]
+    val sampleLocationsBC = sc.broadcast(sampleLocations)
+    val subMatNum = Math.min(Math.max(p/(d/lowD),1),p) // this parameter has to be large enough so that no "ByteStream.hugecapacity" error occurs
+    val rowsPerSubMat = n/subMatNum
+    val partsPerSubMat = rowsPerSubMat/rowsPerPart
+   
+    cfor(0)(i=>i<subMatNum, i=>i+1){i=>
+      val partitions = Array.range(i*partsPerSubMat, (i+1)*partsPerSubMat).toSeq
+      val res = sc.runJob(rawDataSketchRDD,
+          (iter: Iterator[(Int,((BDM[Double], BDM[Double]),(BDV[Double],BDV[Double])))])=>iter.toArray,
+          partitions)
+      val subMat = res.map(x=>x(0)._2._1._1).foldLeft(BDM.zeros[Double](0,lowD))((x,y)=>BDM.vertcat(x,y))
+      val subMatBC = sc.broadcast(subMat)
+      val newSamples = rawDataSketchRDD.map{value=>
+        val buf = new ArrayBuffer[Rating]
+        cfor(0)(j=>j<rowsPerSubMat,j=>j+1){j=>
+            val colnIndex = sampleLocationsBC.value(j+i*rowsPerSubMat)
+            cfor(0)(k=>k<colnIndex.size,k=>k+1){k=>
+              if(colnIndex(k)>=value._1*rowsPerPart && colnIndex(k)<(value._1+1)*rowsPerPart){
+                buf += Rating(j+i*rowsPerSubMat, colnIndex(k), subMatBC.value(j,::) dot value._2._1._2(colnIndex(k)-value._1*rowsPerPart,::))
+              }
+            }
+          }
+        buf
+      }.collect()
+      cfor(0)(m=>m<newSamples.length, m=>m+1){m=>
+        samplesBuf ++= newSamples(m)
       }
-      (v._1, v._2.zip(value))
-    }.toList
-    val samplesRDD = sc.parallelize(samples).flatMap{case (rowIndex, samples)=>
-      samples.map(x=>Rating(rowIndex,x._1, x._2))
-    }.cache()
+    }
+    val samples = samplesBuf.toArray   
+    val samplesRDD = sc.parallelize(samples).cache()
     val samNo = samplesRDD.count
     
     /*
@@ -263,16 +320,17 @@ object OnePassPCA {
     val model22 = ALS.train(samplesRDD, r, Iterations, lambda)
     
     /*
-     * Now we use power method to compute the spectral norm error ||UV^T-A^TB||/||A^TB||.
+     * Now we use AtBv and BtAv to compute the spectral norm error ||UV^T-A^TB||/||A^TB|| via power method
      */
     println(s"Compute the spectral norm ||A^TB||...")
     var normAB = 1.0  //the spectral norm of A^TB 
-    val normal = breeze.stats.distributions.Gaussian(0,1)
-    var v1 = BDV(normal.sample(d).toArray)
+    val rand = new Random(System.currentTimeMillis)
+    var v1 = BDV(Array.fill(n)(rand.nextGaussian))
     normAB = math.sqrt(v1 dot v1)
     v1 :*= 1/normAB
-    cfor(0)(i=>i<5, i=>i+1){i=>
-      v1 = matVecMulti(rawData, v1, n, d, sc)
+    cfor(0)(i=>i<3, i=>i+1){i=>
+      v1 = AtBv(rawData, v1, n, d, sc)
+      v1 = BtAv(rawData, v1, n, d, sc)
       normAB = math.sqrt(v1 dot v1)
       println(math.sqrt(normAB))
       v1 :*= 1/normAB
@@ -282,19 +340,19 @@ object OnePassPCA {
     val userBDM1 = featuresToBDM(model22.userFeatures,n,r)
     val prodBDM1 = featuresToBDM(model22.productFeatures,n,r)   
     var diffNorm = 1.0
-    var v2 = BDV(normal.sample(d).toArray)
+    var v2 = BDV(Array.fill(n)(rand.nextGaussian))
     diffNorm = math.sqrt(v2 dot v2)
     v2 :*= 1/diffNorm
     cfor(0)(i=>i<5, i=>i+1){i=>
-      val v3 = userBDM1*(v2.t*prodBDM1).t
-      v2 = prodBDM1*(v3.t*userBDM1).t - matVecMulti(rawData, v2, n, d, sc)
+      val v3 = userBDM1*(v2.t*prodBDM1).t - AtBv(rawData, v2, n, d, sc)
+      v2 = prodBDM1*(v3.t*userBDM1).t - BtAv(rawData, v3, n, d, sc)
       diffNorm = math.sqrt(v2 dot v2)
       println(math.sqrt(diffNorm))
       v2 :*= 1/diffNorm
     }
     val err = math.sqrt(diffNorm)/math.sqrt(normAB)
     println(s"The achieved spectral norm error is $err")
-    
+
     sc.stop()   
   }
 }
